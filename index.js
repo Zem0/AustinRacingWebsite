7c1b7edd2d552567f31e92b19e22428bc2c340ad
Duncan Horne <duncan.edwardhorne@gmail.com>
2024-08-12 13:08:27 +0100

    Changed the collapsible text block to use the max-height property so I can use a css transition to animate the expanding and collapsing.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ for (i = 0; i < collapsible.length; i++) {
   collapsible[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight === "2000px") {
+      content.style.maxHeight = "0";
     } else {
-      content.style.display = "block";
+      content.style.maxHeight = "2000px";
     }
   });
 }
